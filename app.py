ca06893a249bd306f96c1b3042e492e52b300885
Bimal Patra <134154083+bimal-bp@users.noreply.github.com>
2025-03-16 21:22:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ DB_URL = "postgresql://neondb_owner:npg_Qv3eN1JblqYo@ep-tight-sun-a8z1f6um-poole
 # Gemini API Configuration
 GEMINI_API_KEY = "AIzaSyDWd-ZOM4dy5yLwconHJV6cVkNIoIbWC7g"
 genai.configure(api_key=GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.0-pro')
 
 def get_db_connection():
     return psycopg2.connect(DB_URL)
